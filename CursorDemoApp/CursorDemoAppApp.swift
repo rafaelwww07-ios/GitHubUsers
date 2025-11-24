f4991381c2732f82c7f27e8e121c7fce35b330ea
Rafael Mukhametov <rafaelwww07@mail.ru>
2025-11-24 21:11:41 +0100

    Initial Commit

---
+++ CursorDemoApp/CursorDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorDemoAppApp.swift
+//  CursorDemoApp
+//
+//  Created by Rafael Mukhametov on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CursorDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
